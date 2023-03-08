9769702d32c59019319dad1dd2ec6886c0d8afac
Filip Šišolák <xsisol01@vutbr.cz>
2023-03-08 09:30:07 +0100

    final structure "i hope"

---
+++ xpc-mw5-2023-Artemis/Eshop.DAL/Entities/ProducerEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.DAL.Entities
+{
+    internal class ProducerEntity
+    {
+        //TODO props
+
+    }
+}
