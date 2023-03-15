58d45371db39b621006fdb0d1db160a0d20996ae
fatkh0 <erik.fatkhutdinov@gmail.com>
2023-03-15 15:58:24 +0100

    added fake store db

--- web/app/components/ui/adminMenu/AdminMenu.tsx
+++ web/app/components/ui/adminMenu/AdminMenu.tsx
@@ -8,11 +8,11 @@ import styles from './adminMenu.module.scss'
 const AdminMenu: React.FC = () => {
 
     return (
-            <ul className={styles.adminMenu}>
-                {adminMenuData.map(menuItem => (
-                    <AdminMenuItem key={menuItem.href} href={menuItem.href} text={menuItem.title} />
-                ))}
-            </ul>
+        <ul className={styles.adminMenu}>
+            {adminMenuData.map(menuItem => (
+                <AdminMenuItem key={menuItem.href} href={menuItem.href} text={menuItem.title} />
+            ))}
+        </ul>
     )
 }
 
