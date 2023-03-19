b4126c6c7922c3e74907be4627274371086ce4ee
kikusko77 <christian.kuric@gmail.com>
2023-03-20 00:08:57 +0100

    added fake database

---
+++ xpc-mw5-2023-Artemis/Eshop.webAPI/FakeDB/EshopDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Eshop.webAPI.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Eshop.webAPI.FakeDB
+{
+    public class EshopDbContext : DbContext
+    {
+        public EshopDbContext(DbContextOptions<EshopDbContext> options) : base(options)
+        {
+        }
+
+        public EshopDbContext() : base(new DbContextOptionsBuilder<EshopDbContext>()
+        .UseInMemoryDatabase("EshopInMemoryDatabase")
+            .Options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Add any additional configuration for your models here
+        }
+    }
+
+}
